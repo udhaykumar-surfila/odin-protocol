e489ffcfca6a21d0f42afa6ddd7585c2dbf2e9e9
udhaykumarbala <udhayk97@gmail.com>
2024-05-08 16:24:02 +0530

    images added

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -1,5 +1,7 @@
 import SingleBlur from "./SingleBlur";
 import Footer from "./footer";
+import aravind from "../assets/aravind.jpg";
+import taras from "../assets/taras.jpg";
 
 const Team = () => {
     return (
@@ -17,13 +19,13 @@ const Team = () => {
 
                 <div className="grid items-start max-w-5xl grid-cols-1 px-8 mx-auto mt-12 gap-y-8 sm:grid-cols-3 sm:mt-16 lg:mt-20 gap-x-8 md:gap-x-12 sm:px-0 lg:gap-x-20">
                     <div>
-                        <img className="object-cover w-full rounded-md" src="https://landingfoliocom.imgix.net/store/collection/dusk/images/team/4/team-member-1.png" alt="" />
+                        <img className="object-cover w-full rounded-md" src={aravind} alt="" />
                         <p className="mt-6 text-lg font-semibold text-white text-center">Aravind</p>
                         <p className="mt-1 text-base font-normal text-gray-400 text-center">Founder</p>
                     </div>
 
                     <div className="sm:mt-6 lg:mt-10">
-                        <img className="object-cover w-full rounded-md" src="https://landingfoliocom.imgix.net/store/collection/dusk/images/team/4/team-member-2.png" alt="" />
+                        <img className="object-cover w-full rounded-md" src={taras} alt="" />
                         <p className="mt-6 text-lg font-semibold text-white text-center">Taras</p>
                         <p className="mt-1 text-base font-normal text-gray-400 text-center">CTU</p>
                     </div>
